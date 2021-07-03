35607d7625bb616977dab132b638c2df5dd9b452
t4h4 <tahayasin@outlook.com>
2021-07-03 23:34:51 +0300

    Created IAuthenticationService And LoginDto,  ClientLoginDto

---
+++ DotnetAuthServer.Core/Services/IAuthenticationService.cs
@@ -0,0 +1,21 @@
+﻿using DotnetAuthServer.Core.DTOs;
+using SharedLibrary.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetAuthServer.Core.Services
+{
+    public interface IAuthenticationService
+    {
+        Task<Response<TokenDto>> CreateTokenAsync(LoginDto loginDto);
+
+        Task<Response<TokenDto>> CreateTokenByRefreshToken(string refreshToken);
+
+        Task<Response<NoDataDto>> RevokeRefreshToken(string refreshToken); // canceled token
+
+        Response<ClientTokenDto> CreateTokenByClient(ClientLoginDto clientLoginDto); // client: spa, mobile or mvc
+    }
+}
