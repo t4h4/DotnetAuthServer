a92d2413b41310b05379af84b01d393ea43b9b69
t4h4 <tahayasin@outlook.com>
2021-07-03 13:26:30 +0300

    Created UserRefreshToken entity

---
+++ DotnetAuthServer.Core/Models/UserRefreshToken.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetAuthServer.Core.Models
+{
+    public class UserRefreshToken
+    {
+        public string UserId { get; set; } // RefreshToken owner person id
+        public string Code { get; set; }
+        public DateTime Expiration { get; set; } // token life time
+    }
+}
