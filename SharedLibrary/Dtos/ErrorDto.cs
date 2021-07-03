57d9868c6b3c1a012f0e8cd090783439b05f5bf9
t4h4 <tahayasin@outlook.com>
2021-07-03 15:35:53 +0300

    Created ErrorDto Tüm uygulama yapısının kullanabileceği şekilde SharedLibrary'de ErrorDto oluşturuldu.

---
+++ SharedLibrary/Dtos/ErrorDto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLibrary.Dtos
+{
+    public class ErrorDto
+    {
+        //only in class property set with ctor
+        public List<String> Errors { get; private set; } = new List<string>(); // Colloction; because to be multiple error
+        //only in class property set with ctor
+        public bool IsShow { get; private set; } // show error user or don't show error user.
+
+        //
+
+        public ErrorDto(string error, bool isShow) 
+        {
+            Errors.Add(error);
+            IsShow = isShow;
+        }
+
+        public ErrorDto(List<string> errors, bool isShow)
+        {
+            Errors = errors;
+            IsShow = isShow;
+        }
+    }
+}
