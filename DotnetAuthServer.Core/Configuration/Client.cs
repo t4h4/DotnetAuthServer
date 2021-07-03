5d83b10a4b416857366f1f6d314b771b64e4b532
t4h4 <tahayasin@outlook.com>
2021-07-03 22:36:26 +0300

    Created ITokenService and Client.cs

---
+++ DotnetAuthServer.Core/Configuration/Client.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetAuthServer.Core.Configuration
+{
+    public class Client // spa, mobile or mvc
+    {
+        public string Id { get; set; }
+
+        public string Secret { get; set; }
+
+        //api1.net, api2.net only access
+        public List<String> Audiences { get; set; }
+    }
+}
---
+++ DotnetAuthServer.Core/Services/ITokenService.cs
@@ -0,0 +1,18 @@
+﻿using DotnetAuthServer.Core.Configuration;
+using DotnetAuthServer.Core.DTOs;
+using DotnetAuthServer.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetAuthServer.Core.Services
+{
+    public interface ITokenService
+    {
+        TokenDto CreateToken(UserApp userApp);
+
+        ClientTokenDto CreateTokenByClient(Client client);
+    }
+}
