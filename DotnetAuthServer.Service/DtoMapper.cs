a92c6f37dc9aa47abc43ccf588cf797e92851290
t4h4 <tahayasin@outlook.com>
2021-07-04 16:51:17 +0300

    Added DtoMapper and ObjectMapper

---
+++ DotnetAuthServer.Service/DtoMapper.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using DotnetAuthServer.Core.DTOs;
+using DotnetAuthServer.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetAuthServer.Service
+{
+    internal class DtoMapper : Profile
+    {
+        public DtoMapper()
+        {
+            CreateMap<ProductDto, Product>().ReverseMap();
+            CreateMap<UserAppDto, UserApp>().ReverseMap();
+        }
+    }
+}
---
+++ DotnetAuthServer.Service/ObjectMapper.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetAuthServer.Service
+{
+    public static class ObjectMapper
+    {
+        private static readonly Lazy<IMapper> lazy = new Lazy<IMapper>(() => // ihtiyaca gore, zamani geldiginde lazy.
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<DtoMapper>();
+            });
+
+            return config.CreateMapper();
+        });
+
+        public static IMapper Mapper => lazy.Value; // mapper cagrilana kadar memory'ye yuklenmeyecek, calismayacak. object mapper'in mapper'i cagrildigi anda var config memory'ye yuklenecek. burda lambda ile get yapildi alternatif olarak.
+    }
+}
