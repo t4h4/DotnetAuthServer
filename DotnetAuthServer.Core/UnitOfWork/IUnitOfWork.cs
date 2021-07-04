76c51fe47f205c7ba39c4b1cb33b52bf197dc61c
t4h4 <tahayasin@outlook.com>
2021-07-04 13:41:27 +0300

    Added IUnitOfWork detail image

--- DotnetAuthServer.Core/UnitOfWork/IUnitOfWork.cs
+++ DotnetAuthServer.Core/UnitOfWork/IUnitOfWork.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace DotnetAuthServer.Core.UnitOfWork
 {
+    // Detail: https://www.t4h4.net/wp-content/uploads/2021/07/218166515286125165320.png
     public interface IUnitOfWork
     {
         Task CommmitAsync();
