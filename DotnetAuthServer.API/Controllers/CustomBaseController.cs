2309e28fc781101adb47c2b5601d917ef49b70ea
t4h4 <tahayasin@outlook.com>
2021-07-09 20:13:17 +0300

    Added AuthController

---
+++ DotnetAuthServer.API/Controllers/CustomBaseController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SharedLibrary.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotnetAuthServer.API.Controllers
+{
+    public class CustomBaseController : ControllerBase
+    {
+        public IActionResult ActionResultInstance <T> (Response<T> response) where T : class // generic T alsin.
+        {
+            return new ObjectResult(response) // return status code
+            {
+                StatusCode = response.StatusCode
+            };
+        }
+    }
+}
