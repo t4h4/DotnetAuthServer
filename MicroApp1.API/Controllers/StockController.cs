f4a396f612d283c3cbf4895a13eda488b01c393e
t4h4 <tahayasin@outlook.com>
2021-07-10 20:12:02 +0300

    Added micro api's controller classes

---
+++ MicroApp1.API/Controllers/StockController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace MicroApp1.API.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        public IActionResult GetStock()
+        {
+            var userName = HttpContext.User.Identity.Name;
+
+            var userId = User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier); //FirstOrDefault: ver ya da null donder.
+
+            
+
+            return Ok($" Stock işlemleri  =>  UserName: {userName} - UserId: {userId} ");
+        } 
+    }
+}
---
+++ MicroApp2.API/Controllers/InvoiceController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace MicroApp2.API.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InvoiceController : ControllerBase
+    {
+        public IActionResult GetInvoices()
+        {
+            var userName = HttpContext.User.Identity.Name;
+
+            var userId = User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier);
+
+            
+
+            return Ok($" Invoice işlemleri  =>  UserName: {userName} - UserId: {userId} ");
+        }
+    }
+}
